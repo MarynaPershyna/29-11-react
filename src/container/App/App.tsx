345d31d8d9c98f6c7a1a68e49a91358741b15c17
Maryna <mv15121983@gmail.com>
2023-03-03 19:52:34 +0200

    додали header

--- src/container/App/App.tsx
+++ src/container/App/App.tsx
@@ -1,18 +1,17 @@
 import Footer from 'container/Footer/Footer'
 import Header from 'container/Header/Header'
 import Main from 'container/Main/Main'
-import CssBaseline from '@mui/material/CssBaseline';
-
+import CssBaseline from '@mui/material/CssBaseline'
 
 type Props = {}
 const App = (props: Props) => {
-  return (
-    <>
-        <CssBaseline />
-        <Header />
-        <Main />
-        <Footer />
-    </>
-  )
+    return (
+        <>
+            <CssBaseline />
+            <Header />
+            <Main />
+            <Footer />
+        </>
+    )
 }
 export default App
