bd111e499152492de6c66150f3a10a447b42deab
Maryna <mv15121983@gmail.com>
2023-03-21 15:00:55 +0200

    добавили id продуктов в корзину

--- src/container/App/App.tsx
+++ src/container/App/App.tsx
@@ -7,29 +7,24 @@ import { useState } from 'react'
 
 type Props = {}
 
-type CartData = {
-    totalCount: number
-    totalPrice: number
+type ProductsInCartType = {
+    [id:number]:number
 }
 
 const App = (props: Props) => {
-    const [cartData, setCartData] = useState<CartData>({
-        totalCount: 0,
-        totalPrice: 0,
+    const [productsInCart,setProductsInCsart] = useState<ProductsInCartType>({
+        1: 5,
+        2: 5,
     })
 
     const addProductToCart = (count:number,price:number) => {
-        setCartData((prevState) =>({
-            totalCount: prevState.totalCount + count,
-            totalPrice: prevState.totalPrice + count * price,
-        }))
 
     }
 
     return (
         <StyledEngineProvider injectFirst>
             <CssBaseline />
-            <Header cartData={cartData} />
+            <Header productsInCart={productsInCart} />
             <Main addProductToCart={addProductToCart} />
             <Footer />
         </StyledEngineProvider>
