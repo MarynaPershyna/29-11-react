b7636a887476ca069e4dc349b0ac6cb7ba2a82b0
Maryna <mv15121983@gmail.com>
2023-03-21 10:26:46 +0200

    добав ф-ции для кнопок +-

--- src/components/Products/ProductListItem.tsx
+++ src/components/Products/ProductListItem.tsx
@@ -1,4 +1,11 @@
-import { Button, Card, CardActions, CardContent, TextField } from '@mui/material'
+import {
+    Button,
+    Card,
+    CardActions,
+    CardContent,
+    TextField,
+} from '@mui/material'
+import { useState } from 'react'
 import './ProductListItem.scss'
 
 type Props = {
@@ -18,6 +25,15 @@ const ProductListItem = ({
     price,
     image,
 }: Props) => {
+    const [count, setCount] = useState<number>(1)
+
+    const onIncrement = () => {
+        setCount((prevState) => prevState + 1)
+    }
+    const onDecrement = () => {
+        setCount((prevState) => prevState - 1)
+    }
+
     return (
         <Card variant="outlined" className="product">
             <CardContent>
@@ -31,10 +47,18 @@ const ProductListItem = ({
                 <div className="product-price">
                     Price: <strong>${price}</strong>
                 </div>
-                <div className='product-quantity'>
-                    <Button variant='outlined'>-</Button>
-                    <TextField size='small' value="1"/>
-                    <Button variant='outlined'>+</Button>
+                <div className="product-quantity">
+                    <Button
+                        variant="outlined"
+                        onClick={() => onDecrement()}
+                        disabled={count <= 1}
+                    >
+                        -
+                    </Button>
+                    <TextField size="small" value={count} />
+                    <Button variant="outlined" onClick={() => onIncrement()}>
+                        +
+                    </Button>
                 </div>
 
                 <CardActions className="btns-wrap">
